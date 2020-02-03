4744926250420598668953293a64d07bdbcaf6a1
William Chrapcynski <wchrapcynski@gmail.com>
2020-02-02 20:43:40 -0500

    Navbar, and description box inserted

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Header.css";
+
+function Header() {
+  return (
+    <div className='header'>
+			<div>The Flower Connection</div>
+			<div>Contact</div>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/components/HomePage.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./HomePage.css";
+
+function HomePage() {
+  return (
+    <div className='homepage'>
+      <div className='homepage-description-box'>
+        <img src={require("../images/flowers4.jpg")} />
+        <div className='homepage-description'>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec id
+          aliquam metus. Nam blandit, quam ac luctus pharetra, sem justo
+          suscipit mi, vel ornare ex arcu a diam. Nullam eu tincidunt nisi, nec
+          egestas lectus. Integer a elit nec arcu mollis laoreet.
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
